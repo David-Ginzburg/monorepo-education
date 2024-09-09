d1bc1b50372c35e2800e276ae694787453500a4a
Vadim Mostovenko <mostovenko.vadim@wb.ru>
2024-09-09 20:54:37 +0300

    add validation

--- libs/contracts/src/lib/account/account.register.ts
+++ libs/contracts/src/lib/account/account.register.ts
@@ -1,9 +1,17 @@
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
 export namespace AccountRegister {
   export const topic = 'account.register.command';
 
   export class Request {
+    @IsEmail()
     email: string | undefined;
+
+    @IsString()
     password: string | undefined;
+
+    @IsOptional()
+    @IsString()
     displayName?: string;
   }
 
